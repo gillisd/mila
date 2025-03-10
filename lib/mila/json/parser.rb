37214221285dbc0940ef0ffc70aaeed5d612b8d2
David Gillis <david@flipmine.com>
2025-03-10 12:29:25 -0500

    Use stock json as default

--- lib/mila/json/parser.rb
+++ lib/mila/json/parser.rb
@@ -1,10 +1,7 @@
 module Mila
   module JSON
     class Parser
-      # ::MultiJson.use Mila::JSON::Parser::MultiJson::Adapters::Bundled
-      ::MultiJson.use :json_common
-
-      def initialize(parser = :multi_json)
+      def initialize(parser = :default)
         @strategy = StrategyProvider.instance.parser_strategy(parser)
       end
 
